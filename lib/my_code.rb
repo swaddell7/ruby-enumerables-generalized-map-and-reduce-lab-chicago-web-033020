bc641cb9bb66b5f477d35ddfa2520cc021b29579
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-19 20:47:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,11 @@
 # Your Code Here
-def map
-  
+def map(source_array)
+  index = 0
+  new_array = 0
+  while index < source_array.length do
+    yield
+    new_array << source_array[index]
+    index += 1
+  end
+  new_array
+end
