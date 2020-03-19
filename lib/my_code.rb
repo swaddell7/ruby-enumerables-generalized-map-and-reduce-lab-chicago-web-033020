5517dd64cc5c8cfb57950c959b07b0c6b528a913
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-19 20:52:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,11 @@ def map(source_array)
   end
   new_array
 end
+
+def reduce(source_array, starting_point = 0)
+  index = 0 
+  while index < source_array.length do 
+    yield
+    index += 1
+  end
+end
