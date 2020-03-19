30347d8183f6c813c19ed53fa4b6f2ca35d24049
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-19 22:00:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(source_array)
+  index = 0
+  new_array = []
+  while index < source_array.length do
+    new_array.push(yield(source_array[index]))
+    index += 1
+  end
+  new_array
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    index = 0
+  else
+    sum = array[0]
+    index = 1
+  end
+  while index < array.length do 
+    sum = yield(sum, array[index])
+    index += 1 
+  end
+  sum
+end
