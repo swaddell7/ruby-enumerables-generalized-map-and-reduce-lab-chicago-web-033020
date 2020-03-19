0fa4d295010998a8d1694e46e5809d3b8c302b83
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-19 21:38:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # Your Code Here
 def map(source_array)
   index = 0
-  new_array = 0
+  new_array = []
   while index < source_array.length do
     new_array.push(yield(source_array[index]))
     index += 1
